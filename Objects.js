af742169e7c1573783c269c3c772729513b70498
MarkF0X <valentin.alexandrovich.e@gmail.com>
2023-02-01 10:24:05 +0300

    practice-17, динамическое имя свойства, методы добавления и удаления контактов

---
+++ Objects.js
@@ -0,0 +1,24 @@
+const TGBook = {
+    list: {
+        'Ksu S': 123123,
+        'Ksu V': 321321,
+        'Ant M': 231231
+    },
+    add(name, number) {
+        this.list[name] = number;
+    },
+    delete(name) {
+        delete this.list[name];
+    },
+    log() {
+        console.log(this.list)
+    }
+};
+
+
+TGBook.add('Max', 132132);
+console.log(TGBook.list['Max']);
+TGBook.log();
+TGBook.delete('Max');
+console.log(TGBook.list['Max']);
+TGBook.log();
