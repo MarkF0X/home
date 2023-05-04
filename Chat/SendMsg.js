f7050e13f885a049d6ad368211765ece78f78b71
MarkF0X <valentin.alexandrovich.e@gmail.com>
2023-05-04 13:06:08 +0300

    Чат. Вёрстка и функция отправки сообщений через template

---
+++ Chat/SendMsg.js
@@ -0,0 +1,11 @@
+function sentMessage() {
+    const dialog = document.getElementById('dialog-window');
+
+    let newSentMessage = document.createElement('div');
+    newSentMessage.setAttribute('class', 'sent-message');
+    const tmplt = document.getElementById('template-sent-message');
+    newSentMessage.append(tmplt.content.cloneNode(true));
+    newSentMessage.firstChild.nextSibling.textContent = document.getElementById('text-area').value;
+    newSentMessage.lastChild.previousSibling.textContent = `${new Date().getHours()}:${new Date().getMinutes()}`;
+    dialog.append(newSentMessage);
+}
